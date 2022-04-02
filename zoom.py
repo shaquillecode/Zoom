1ebea09370530fcaaaf694340ea66f4efc13763c
shaquillecode <84408174+shaquillecode@users.noreply.github.com>
2022-04-02 03:35:35 -0400

    modified

--- zoom.py
+++ zoom.py
@@ -1,49 +1,63 @@
-"""Zoom Link Bot """
-# Some core and some installed packages/modules
-# If I have buttons to click on the screen,
-# I will have to leverage another module that you have to install using pip called "pyautogui".
+'''zoom.py'''
 import time
 import webbrowser
 import schedule
 
 
-def zoom_bot():
-    """This will open the Zoom Invite Link"""
-    link = "https://dfa.zoom.us/j/87183477990"
-    webbrowser.open(f"{link}")
-
-def aws_course():
-    """This will open Amazon web services academy on the webbrowser"""
-    awsacademy = "https://awsacademy.instructure.com/courses/9010/modules"
-    webbrowser.open(f"{awsacademy}")
-
-def sw_course():
-    """This will open StudioWeb on the webbrowser"""
-    studioweb = "https://school.studioweb.com/"
-    webbrowser.open(f"{studioweb}")
 
 def open_link(link):
-    """ This will open a link on the webbrowser """
+    '''
+    Pass in any link to this function
+    Have it open that link on the webbrowser.
+    '''
     webbrowser.open(link)
 
-# zoom_bot()
-# aws_course()
-# sw_course
-CLASS = "https://dfa.populiweb.com/internal/common/home.php"
-open_link(CLASS)
 
+def course():
+    '''
+    Pass in any link to this function
+    Have it open that link on the webbrowser.
+    '''
+    academy = "https://"
+    webbrowser.open(f"{academy}")
+
+def foundations():
+    '''
+    Pass in any link to this function
+    Have it open that link on the webbrowser.
+    '''
+    #Zoom Invite Link goes here:
+    zoom_link = "https://"
+
+    #This will open your Zoom Invite Link
+    webbrowser.open(f"{zoom_link }")
+
+
+POPULI = "https://"
+open_link(POPULI)
+course()
+foundations()
 
 #Schedule your function to run at a certain time.
-schedule.every().monday.at("10:30").do(zoom_bot())
-schedule.every().tuesday.at("10:30").do(zoom_bot())
-schedule.every().wednesday.at("10:30").do(zoom_bot())
-schedule.every().thursday.at("10:30").do(zoom_bot())
+
+schedule.every().monday.at("10:30").do(foundations())
+schedule.every().tuesday.at("10:30").do(foundations())
+schedule.every().wednesday.at("10:30").do(foundations())
+schedule.every().thursday.at("10:30").do(foundations())
+schedule.every().friday.at("10:30").do(foundations())
 
 
-#What am I gonna do if it's not the scheduled time?
+#What if it's not the scheduled time?
+
 #Create an infinite loop that will run until the time is met.
+FLAG = True
+NUM = 0
 
-while True:
-    schedule.run_pending()
-    print("Not Yet...")
-    time.sleep(1)
+while FLAG:
+    if NUM == 3600:
+        FLAG = False
+    else:
+        schedule.run_pending()
+        print("Not Yet...")
+        time.sleep(1)
+        NUM += 1
